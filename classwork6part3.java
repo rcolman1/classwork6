9bb1d47e6f0b41543db15ae8f14d4bac8229dca6
rcolman1 <52420838+rcolman1@users.noreply.github.com>
2019-09-23 12:44:38 -0700

    Add files via upload

---
+++ classwork6part3.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.Random;
+public class classwork6part3
+{
+    public static void main(String[] args)
+    {
+        System.out.println(doesCaraHaveASTHMA(0));
+        
+    
+    }
+    
+    public static String doesCaraHaveASTHMA(int a)
+    {
+        if (a >= 10)
+        return ("NO you have no ASTHMA!!!!!!!!!!!!!!!!!!!!");
+        
+        else
+        return ("YES YOU HAVE TERRIBLE RESPITORY ASTHMA THAT WILL KILL YOU EVENTUALLY!!!!!!!!!!!!!!!");
+        
+    }
+}
---
+++ classwork6part4.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.Random;
+public class classwork6part4
+{
+    public static void main(String[] args)
+    {
+        System.out.println(name("Rhys"));
+        
+    
+    }
+    
+    public static String name(String name)
+    {
+        if (name.equals("Rhys") || name.equals("rhys"))
+        return ("Yes, you have the coolest name.");
+        
+        else
+        return ("NO");
+    }
+}
+
